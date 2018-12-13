4604a0b0b486fce635d37755fa6da92b466c6894
JoshuaDESKTOP <jc.grant22@gmail.com>
2018-12-13 09:04:07 -0800

    Made AbstractResourceLibrary generic, fixed tests

--- ResourceLibrary/AbstractResourceLibrary.cs
+++ ResourceLibrary/AbstractResourceLibrary.cs
@@ -1,23 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using TUFFYCore.Service;
+using UnityEngine;
 
 /*
  * AbstractResourceLibrary provides functionality to load in and retrieve content.
  */
 namespace TUFFYCore.ResourceLibrary
 {
-    public abstract class AbstractResourceLibrary : AbstractService
+    public abstract class AbstractResourceLibrary<T> : AbstractService
     {
 
         protected string Prefix = "";
         protected string Path;
         protected Type Type;
 
+        protected Dictionary<string, T> _contents;
 
-        protected AbstractResourceLibrary(Type type, string path)
+
+        protected AbstractResourceLibrary(string path)
         {
-            Type = type;
+            Type = typeof(T);
             Path = path;
+            _contents = new Dictionary<string, T>();
         }
 
         public void SetPrefix(string prefix)
@@ -32,6 +37,30 @@ namespace TUFFYCore.ResourceLibrary
         }
 
         public abstract void LoadResources();
+
+
+        public Dictionary<string, T> GetCatalogue()
+        {
+            return _contents;
+        }
+
+        public T Get(string id)
+        {
+            if (Contains(id)) return _contents[id];
+
+            return default(T);
+        }
+
+        public int CountMembers()
+        {
+            return _contents.Count;
+        }
+
+
+        public bool Contains(string id)
+        {
+            return _contents.ContainsKey(id);
+        }
 
     }
 }
