bee92d40c0a63e3973216da2745f4e453ce2e4c4
joshuacassidygrant <jc.grant22@gmail.com>
2019-01-01 21:15:12 -0500

    Resource Module basics with basic test

---
+++ Plugins/ResourceModule/ResourceModule.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TofuCore.Events;
+using UnityEngine;
+
+
+namespace TofuPlugin.ResourceModule
+{
+    public class ResourceModule
+    {
+
+
+        public readonly string Name;
+        
+
+        public int IValue => Mathf.RoundToInt(_value);
+        public int IMax => Mathf.RoundToInt(_max);
+        public float FValue => _value;
+        public float FMax => _max;
+
+        public float Percent
+        {
+            get
+            {
+                if (_max == 0) return 0;
+                return _value / _max;
+            }
+        }
+
+        private float _value;
+        private float _max;
+        private readonly EventContext _eventContext;
+
+        public ResourceModule(string name, float max, float val, EventContext eventContext)
+        {
+            Name = name;
+            _value = val;
+            _max = max;
+            _eventContext = eventContext;
+        }
+
+        public void Deplete(float amount, string depletionEventKey = null, EventPayload depletionPayload = null)
+        {
+            _value -= amount;
+            if (depletionEventKey != null && _value <= 0)
+            {
+                _eventContext.TriggerEvent(depletionEventKey, depletionPayload);
+            }
+        }
+
+        public bool Spend(float amount)
+        {
+            if (CanSpend(amount))
+            {
+                _value -= amount;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool CanSpend(float amount)
+        {
+            return _value - amount >= 0;
+        }
+
+        public void SetMax(float amount)
+        {
+            _max = amount;
+        }
+
+    }
+
+}
---
+++ Plugins/ResourceModule/Test/ResourceModuleTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TofuCore.Events;
+using TofuPlugin.ResourceModule;
+using UnityEngine;
+
+public class ResourceModuleTests {
+
+    private EventContext _eventContext;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _eventContext = new EventContext();
+        
+    }
+
+    [Test]
+    public void TestResourceModuleShouldBeConstructed()
+    {
+        ResourceModule resourceModule = new ResourceModule("Test", 100f, 40f, _eventContext);
+
+        Assert.AreEqual("Test", resourceModule.Name);
+        Assert.AreEqual(100f, resourceModule.FMax);
+        Assert.AreEqual(40f, resourceModule.FValue);
+        Assert.AreEqual(100, resourceModule.IMax);
+        Assert.AreEqual(40, resourceModule.IValue);
+        Assert.AreEqual(0.4f, resourceModule.Percent);
+
+
+
+    }
+
+    [Test]
+    public void TestResourcePercentReturns0WhenMaxIs0()
+    {
+        ResourceModule resourceModule = new ResourceModule("Test", 0, 40f, _eventContext);
+        
+        Assert.AreEqual(0, resourceModule.Percent);
+    }
+
+    [Test]
+    public void TestEventSentWhenTriggeredOnDepletion()
+    {
+        //TODO: this
+    }
+
+
+}
