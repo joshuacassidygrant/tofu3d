19f8a494c94bf4b192039f623209e43e2d576cb5
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-01-16 09:13:23 -0800

    Started faction module

---
+++ Plugins/Agents/Faction/Faction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TofuCore.Glop;
+using TofuCore.Service;
+using UnityEngine;
+
+namespace TofuPlugin.Agents.Faction
+{
+    public class Faction: Glop
+    {
+
+        //Who controls this allegiance
+
+        //List of other allegiances and their relationships
+        private Dictionary<Faction, int> _relationships;
+
+        public Faction(string idName, int id, ServiceContext serviceContext) : base(id, idName, serviceContext)
+        {
+            _relationships = new Dictionary<Faction, int>();
+        }
+
+        public void SetMutualRelationship (Faction faction, int value)
+        {
+            SetRelationship(faction, value);
+            faction.SetRelationship(faction, value);
+
+        }
+
+        public void SetRelationship(Faction faction, int value)
+        {
+            if (!_relationships.ContainsKey(faction))
+            {
+                _relationships.Add(faction, value);
+            }
+            else
+            {
+                _relationships[faction] = value;
+            }
+        }
+
+        public override void Update(float frameDelta)
+        {
+            //Do something!
+        }
+    }
+
+}
+
---
+++ Plugins/Agents/Faction/FactionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TofuCore.Glop;
+using UnityEngine;
+
+namespace TofuPlugin.Agents.Faction
+{
+    public class FactionManager : GlopManager
+    {
+
+        //Set breakpoints for certain faction level interactions
+        public FactionManager()
+        {
+        }
+    }
+
+}
