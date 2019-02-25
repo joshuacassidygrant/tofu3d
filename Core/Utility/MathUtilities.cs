e06d16109653db0ad35446ab7aff4819d539b0c1
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-02-25 09:08:31 -0800

    Added floor to v3int utility

--- Core/Utility/MathUtilities.cs
+++ Core/Utility/MathUtilities.cs
@@ -9,6 +9,11 @@ namespace TofuCore.Utility
             return new Vector3Int(Mathf.RoundToInt(v3.x), Mathf.RoundToInt(v3.y), Mathf.RoundToInt(v3.z));
         }
 
+        public static Vector3Int RoundDownToVector3Int(Vector3 v3)
+        {
+            return new Vector3Int(Mathf.FloorToInt(v3.x), Mathf.FloorToInt(v3.y), Mathf.FloorToInt(v3.z));
+        }
+
         public static Vector3 Vector3IntToVector3(Vector3Int v3i)
         {
             return new Vector3(v3i.x, v3i.y, v3i.z);
