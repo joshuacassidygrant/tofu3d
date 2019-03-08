e34e45e83b4bbf100c0f0fa654ee85fe9d10d2d0
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-03-08 07:40:38 -0800

    Added initialization templates :

---
+++ Core/Initialization/AbstractGameServiceInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TofuCore.Service
+{
+    public abstract class AbstractGameServiceInitializer : MonoBehaviour
+    {
+
+        private GameObject _cameraObject;
+
+        protected ServiceContext ServiceContext;
+
+        void Awake()
+        {
+            ServiceContext = new ServiceContext();
+
+            HandleUnityObjects();
+            RegisterPayloads();
+            BindServices();
+            InitServices();
+        }
+
+        protected abstract void HandleUnityObjects();
+        protected abstract void RegisterPayloads();
+        protected abstract void BindServices();
+        protected abstract void InitServices();
+
+        //For testing purposes
+        public ServiceContext GetServiceContext()
+        {
+            return ServiceContext;
+        }
+
+    }
+}
+
---
+++ Core/Initialization/IContextConfigurator.cs
@@ -0,0 +1,13 @@
+﻿namespace TofuCore.Service
+{
+    /*
+     * Takes in a service context, modifies, and returns it. Avoid other side effects.
+     */ 
+    public interface IContextConfigurator
+    {
+
+        void Configure(ServiceContext context);
+
+    }
+
+}
