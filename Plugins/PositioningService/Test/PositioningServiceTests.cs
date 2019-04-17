32acb0c894071cff0dd1a6863844c8c71e3122ff
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-04-17 09:03:31 -0700

    Stubbed positioning service tests

---
+++ Plugins/PositioningService/Test/PositioningServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace TofuTest.PositioningServiceTest
+{
+    public class PositioningServiceTests
+    {
+        [Test]
+        public void Stub()
+        {
+            //TODO
+            Assert.Pass();
+        }
+    }
+
+}
