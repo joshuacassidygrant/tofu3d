61ef1e3e0ebdbe2499089ddf9eaa03a0e87385a6
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-08-02 10:43:01 -0700

    Added position to tangible rednerer

--- Plugins/Renderables/ITangibleRenderer.cs
+++ Plugins/Renderables/ITangibleRenderer.cs
@@ -1,10 +1,13 @@
-﻿using TofuCore.Tangible;
+﻿
+using TofuCore.Tangible;
+using UnityEngine;
 
 //Represents an ITangible object in the world and allows querying and targeting that object through UI.
 
 public interface ITangibleRenderer 
 {
     ITangible Tangible { get; }
+    Vector3 Position { get; }
     void ShowTargetableHighlight(bool on);
     
 }
