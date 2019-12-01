44f05f5c90f1db2dd24cc07f6b22802240330262
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-11-30 22:31:56 -0800

    Added an event triggering button!

---
+++ Plugins/UI/TofuUIButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TofuPlugin.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TofuPlugin.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class TofuUIButton : TofuUiBase
+    {
+
+        public string EventName;
+
+        private Button _button;
+
+        void Start()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(OnClick);
+            BindServiceContext();
+        }
+
+        private void OnClick()
+        {
+            EventContext.TriggerEvent(EventName);
+        }
+
+
+    }
+}
+
